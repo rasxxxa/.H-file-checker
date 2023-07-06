afdbdc5f2863e5a6dab9d5e88d326736d6b56552
Radoica Nikolic <radoica.nikolic@fazi.rs>
2023-07-06 14:11:38 +0200

    Added method check and write

--- cplusplusparser/Source.cpp
+++ cplusplusparser/Source.cpp
@@ -8,6 +8,22 @@
 #include <unordered_map>
 #include <filesystem>
 
+struct ClassParser
+{
+    std::set<std::string> unique_variables;
+    std::set<std::string> private_methods;
+    std::set<std::string> public_methods;
+    std::set<std::string> static_methods;
+    std::string class_name;
+    std::string global_pointer;
+};
+
+enum class Visibility
+{
+    Private,
+    Public
+};
+
 std::vector<std::string> ReadFile(const std::string_view& filePath)
 {
     std::vector<std::string> file;
@@ -168,14 +184,14 @@ bool IsClass(const std::string& line)
 }
 
 
-std::vector<std::string> ParseVariable(const std::string& line)
+std::vector<std::string> ParseVariable(const std::string& line, ClassParser& parser)
 {
     std::string variable;
     if (!line.contains(";"))
         return std::vector<std::string>();
 
     std::vector<std::string> variables;
-
+    bool isExtern = line.find("extern") != std::string::npos;
     int pos = static_cast<int>(line.find(";") - 1);
     int posLeft = 0;
     int posRight = -1;
@@ -218,7 +234,9 @@ std::vector<std::string> ParseVariable(const std::string& line)
             pos--;
 
         variables.push_back(line.substr(pos + 1, posRight - pos));
-        std::string restOfLine = line.substr(0, pos);
+        if (isExtern)
+            parser.global_pointer = variables.back();
+    	std::string restOfLine = line.substr(0, pos);
 
         if (restOfLine.back() - 1 == ',' || restOfLine.back() - 2 == ',' || restOfLine.back() == ',')
             pos = restOfLine.find_last_of(',') - 1;
@@ -271,18 +289,30 @@ void RemoveNamespaces(std::vector < std::string>& lines)
 }
 
 
-void RemoveMethods(std::vector<std::string>& lines, std::vector<std::string>& returnMethods)
+void RemoveMethods(std::vector<std::string>& lines, std::vector<std::string>& returnMethods, ClassParser& parser)
 {
     size_t pos = 0;
+    Visibility defaultVis = Visibility::Private;
     std::vector<std::string> withoudMethods;
     while (pos < lines.size())
     {
+        if (lines[pos].contains("private:") || lines[pos].contains("protected:"))
+            defaultVis = Visibility::Private;
+        else if (lines[pos].contains("public:"))
+            defaultVis = Visibility::Public;
+
+
         if (!lines[pos].contains("("))
         {
             withoudMethods.push_back(lines[pos]);
         }
         else
         {
+            bool isStatic = false;
+            if (lines[pos].contains("static"))
+            {
+                isStatic = true;
+            }
             auto posOfParentesis = lines[pos].find("(") -1;
             std::string Method = "";
             while (posOfParentesis >= 0 && lines[pos][posOfParentesis] != ' ')
@@ -292,6 +322,28 @@ void RemoveMethods(std::vector<std::string>& lines, std::vector<std::string>& re
             }
             std::reverse(Method.begin(), Method.end());
             returnMethods.push_back(Method);
+
+            const static auto FilterMethod = [](const std::string& line_to_check)
+            {
+                return !line_to_check.contains("operator") && !line_to_check.contains("=") && !line_to_check.contains("&&") && !line_to_check.contains("~");
+            };
+
+            if (FilterMethod(Method))
+            {
+                const auto it = std::remove_if(Method.begin(), Method.end(), [](char a)
+                {
+                        return a == '&' || a == ' ' || a == '*';
+                });
+                Method.erase(it, Method.end());
+                if (defaultVis == Visibility::Private)
+                    parser.private_methods.insert(Method);
+                else
+                    parser.public_methods.insert(Method);
+
+                if (isStatic)
+                    parser.static_methods.insert(Method);
+            }
+            
             while (pos < lines.size() && (!lines[pos].contains(")")))
                 pos++;
         }
@@ -302,26 +354,17 @@ void RemoveMethods(std::vector<std::string>& lines, std::vector<std::string>& re
 
 
 
-std::set<std::string> GetUniqueVariables(std::vector<std::string>& lines)
+ClassParser GetUniqueVariables(std::vector<std::string>& lines)
 {
+    ClassParser parser;
+
     std::set<std::string> unique_variables;
     std::set<std::string> methods;
     std::set<std::string> structs;
     std::vector<std::string> linesWithoutComments;
-    // delete all comments 
-    RemoveEnums(lines);
-    
+
     for (const auto& line : lines)
     {
-        if (IsNewLineOrEmpty(line))
-            continue;
-
-        if (IsPrivateProtectedOrPublic(line))
-            continue;
-
-        if (IsClass(line))
-            continue;
-
         std::string formatted;
         if (!IsCommentLine(line, formatted))
         {
@@ -331,29 +374,57 @@ std::set<std::string> GetUniqueVariables(std::vector<std::string>& lines)
         if (!formatted.empty())
             linesWithoutComments.push_back(formatted);
     }
-    structs = FindAllStructs(linesWithoutComments);
-    std::vector<std::string> methodNames;
-    RemoveMethods(linesWithoutComments, methodNames);
-    RemoveNamespaces(linesWithoutComments);
 
-    //for (auto val : linesWithoutComments)
-    //{
-    //    std::cout << val << std::endl;
-    //}
-    //
-    for (const auto& val : linesWithoutComments)
+    std::vector<std::string> linesWithoutNewLines;
+
+    for (const auto& line : linesWithoutComments)
+    {
+        if (IsNewLineOrEmpty(line))
+            continue;
+
+        if (IsClass(line))
+        {
+            if (parser.class_name.empty())
+            {
+                size_t pos = line.find_first_not_of("class");
+                if (pos != std::string::npos)
+                {
+                    while (pos != line.size() && (!std::isalpha(line[pos])) || line[pos] == '_') pos++;
+                    std::string className;
+                    while (std::isalpha(line[pos]) || line[pos] == '_') { className.push_back(line[pos]); pos++; }
+                    if (!className.empty()) parser.class_name = className;
+                }
+            }
+            continue;
+        }
+        linesWithoutNewLines.push_back(line);
+    }
+
+
+    RemoveEnums(linesWithoutNewLines);
+
+	structs = FindAllStructs(linesWithoutNewLines);
+
+    std::vector<std::string> methodNames;
+    RemoveMethods(linesWithoutNewLines, methodNames, parser);
+    RemoveNamespaces(linesWithoutNewLines);
+
+    for (const auto& val : linesWithoutNewLines)
     {
         std::string copy = val;
-        auto vars = ParseVariable(copy);
-        if (vars.size())
+        auto vars = ParseVariable(copy, parser);
+        if (!vars.empty())
         {
             for (auto valS : vars)
                 unique_variables.insert(valS);
         }
     }
-
-
-    return unique_variables;
+    parser.unique_variables = unique_variables;
+    if (!parser.global_pointer.empty())
+    {
+        parser.unique_variables.erase(parser.global_pointer);
+    }
+    return parser;
 }
 
 
@@ -376,14 +447,14 @@ void ClearCppFileFromComments(std::vector<std::string>& lines)
 }
 
 
-std::unordered_map<std::string, int> GetOccurenceOfVariables(const std::set<std::string>& variables, const std::vector<std::string>& lines)
+std::unordered_map<std::string, int> GetOccurenceOfVariables(const ClassParser& parser, const std::vector<std::string>& lines)
 {
     std::unordered_map<std::string, int> occurences;
 
-    for (const auto& val : variables)
+    for (const auto& val : parser.unique_variables)
         occurences[val] = 0;
 
-    for (const auto& val : variables)
+    for (const auto& val : parser.unique_variables)
     {
         for (const auto& line : lines)
         {
@@ -395,12 +466,92 @@ std::unordered_map<std::string, int> GetOccurenceOfVariables(const std::set<std:
     return occurences;
 }
 
+
+std::unordered_map<std::string, int> GetMethodOcurence(const ClassParser& parser, const std::vector<std::vector<std::string>>& files)
+{
+    std::unordered_map<std::string, int> ocurences;
+    struct MethodPossible
+    {
+        std::string key_name;
+        std::vector<std::string> possible_names;
+    };
+    std::vector<MethodPossible> all_combination;
+
+    for (const auto& line : parser.public_methods)
+    {
+        ocurences[line] = 0;
+        MethodPossible method_possible;
+        method_possible.key_name = line;
+        if (!parser.global_pointer.empty())
+        {
+            std::string fullName = parser.global_pointer;
+            fullName.append("->");
+            fullName.append(line);
+            method_possible.possible_names.push_back(fullName);
+        }
+        else if (!parser.class_name.empty())
+        {
+            std::string fullName = parser.class_name;
+            fullName.append("::");
+            fullName.append(line);
+            method_possible.possible_names.push_back(fullName);
+        }
+        if (!method_possible.possible_names.empty())
+            all_combination.push_back(method_possible);
+    }
+
+    for (const auto& line : parser.static_methods)
+    {
+        ocurences[line] = 0;
+        MethodPossible method_possible;
+        method_possible.key_name = line;
+        if (!parser.global_pointer.empty())
+        {
+            std::string fullName = parser.global_pointer;
+            fullName.append("->");
+            fullName.append(line);
+            method_possible.possible_names.push_back(fullName);
+        }
+
+        if (!parser.class_name.empty())
+        {
+            std::string fullName = parser.class_name;
+            fullName.append("::");
+            fullName.append(line);
+            method_possible.possible_names.push_back(fullName);
+        }
+
+        if (!method_possible.possible_names.empty())
+            all_combination.push_back(method_possible);
+    }
+
+    for (const auto& possible_combination : all_combination)
+    {
+	    for (const auto& file : files)
+	    {
+		    for (const auto& lineInFile : file)
+		    {
+			    for (const auto& combination : possible_combination.possible_names)
+			    {
+				   if (lineInFile.contains(combination))
+				   {
+                       ocurences[possible_combination.key_name]++;
+				   }
+			    }
+		    }
+	    }
+    }
+
+    return ocurences;
+}
+
+
 #define WRITE_TO_FILE
 
 int main(int argc, const char** argv) 
 {
 #ifdef _DEBUG
-    std::string filesPath = "D:\\magicnhd\\Reel5CO";
+    std::string filesPath = "D:\\magicnhd\\BiCa61A";
     std::string s = "";
     if (argc > 1)
         s = argv[1];
@@ -416,6 +567,8 @@ int main(int argc, const char** argv)
             std::string hFile;
             std::string cppFile;
         };
+
+        std::unordered_map<std::string, ClassParser> mapped_parsers;
 
         std::vector<Files> toCheck;
 
@@ -436,6 +589,7 @@ int main(int argc, const char** argv)
             }
         }
         std::ofstream file("UnusedVariables.txt");
+        std::unordered_map <std::string, std::vector < std::string >> allFiles;
         for (const auto& val : toCheck)
         {
             
@@ -451,9 +605,11 @@ int main(int argc, const char** argv)
             auto linesH = ReadFile(val.hFile);
             auto linesCpp = ReadFile(val.cppFile);
 
-            auto values = GetUniqueVariables(linesH);
+            auto parser = GetUniqueVariables(linesH);
             ClearCppFileFromComments(linesCpp);
-            auto occurence = GetOccurenceOfVariables(values, linesCpp);
+            allFiles[val.hFile] = (linesCpp);
+            mapped_parsers[val.hFile] = parser;
+            auto occurence = GetOccurenceOfVariables(parser, linesCpp);
 
             struct mapval
             {
@@ -468,7 +624,8 @@ int main(int argc, const char** argv)
                 mapval m;
                 m.s = val.first;
                 m.occ = val.second;
-                valuesM.push_back(m);
+                if (m.occ <= 5)
+                    valuesM.push_back(m);
             }
 
             std::sort(valuesM.begin(), valuesM.end(), [](const mapval& first, const mapval& second) {
@@ -488,6 +645,46 @@ int main(int argc, const char** argv)
 
 
         }
+
+        for (const auto& check_all_parsers : mapped_parsers)
+        {
+            std::vector<std::vector<std::string>> all_files;
+            for (const auto& lines : allFiles)
+            {
+                all_files.push_back(lines.second);
+            }
+
+            auto usage = GetMethodOcurence(check_all_parsers.second, all_files);
+
+            struct mapvalS
+            {
+                std::string s;
+                int occ;
+            };
+            std::vector<mapvalS> sortedMethods;
+            for (const auto& v : usage)
+            {
+                mapvalS m;
+                m.s = v.first;
+                m.occ = v.second;
+                if (m.occ <= 5)
+                    sortedMethods.push_back(m);
+            }
+
+            std::sort(sortedMethods.begin(), sortedMethods.end(), [](const mapvalS& first, const mapvalS& second) {
+
+                return first.occ < second.occ;
+
+                });
+
+            file << std::endl << std::endl << std::endl << "Class : " << check_all_parsers.first << std::endl;
+            for (const auto& methods : sortedMethods)
+            {
+                file << "Method " << methods.s << " - ocurence : " << methods.occ<< " times!" << std::endl;
+            }
+
+        }
+
         file.flush();
         file.close();
     }
@@ -495,8 +692,8 @@ int main(int argc, const char** argv)
     {
         std::string hFile, cppFile;
 #ifdef _DEBUG
-        hFile = "VStatus.h";
-        cppFile = "VStatus.cpp";
+        hFile = "DBingoCards.h";
+        cppFile = "DBingoCards.cpp";
 #else
         hFile = argv[1];
         cppFile = argv[2];
